490b84205149a5e820e2c36834e89f8133628977
seanchok <seanchok@gmail.com>
2020-08-19 20:03:58 +0800

    fix postcss purge css with safelist

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+
+  // Indicate which selectors are safe to leave
+  safelist: ['html', 'body']
 });
 
 module.exports = {

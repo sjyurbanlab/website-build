03e21ca9449535482bf95370b0bcd5235d6ef2b1
seanchok <seanchok@gmail.com>
2020-09-05 12:33:46 +0800

    fix images, and build video player component

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -3,10 +3,12 @@ import * as GenericAppComponents from '@components/generic/app';
 import { Section } from './Section';
 import { Link } from './Link';
 import { Button } from './Button';
+import { VideoPlayer } from './VideoPlayer';
 
 export const mdxComponents = {
   ...GenericAppComponents,
   Section,
   Link,
   Button,
+  VideoPlayer,
 };

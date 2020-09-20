08cef1e891660ff212eb0beb5362df225d688541
seanchok <seanchok@gmail.com>
2020-09-20 17:59:33 +0800

    build alumni members page
    move original members page to present members page
    
    various minor bug fixes and refactoring

---
+++ src/assets/data/members/alumnus-group.data.ts
@@ -0,0 +1,27 @@
+import rawAlumni from './alumnus-groups.json';
+import { MemberGroup } from '@src/types/memberGroup';
+
+export const alumnusGroupsData: MemberGroup[] = Object.entries(rawAlumni).map(
+  ([groupName, memberGroup]: [string, any]) => ({
+    groupName,
+    members: Object.entries(memberGroup)
+      .map(([key, member]: [string, any]) => ({
+        id: Number(key),
+        avatar: member.avatar,
+        firstName: member.firstName,
+        lastName: member.lastName,
+        salutation: member.salutation,
+        position: member.position,
+        email: member.email,
+        introduction: member.introduction,
+        affiliationPeriodStart: member.affiliationPeriodStart
+          ? new Date(member.affiliationPeriodStart)
+          : undefined,
+        affiliationPeriodEnd: member.affiliationPeriodEnd
+          ? new Date(member.affiliationPeriodEnd)
+          : undefined,
+        cv: member.cv,
+      }))
+      .sort((a, b) => a.id - b.id),
+  })
+);
--- src/pages/members/alumni.tsx
+++ src/pages/members/alumni.tsx
@@ -1,48 +1,44 @@
 import React, { createRef, useRef } from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { OtherMembersMdxQuery } from '../../graphql-types';
+import { OtherAlumniMdxQuery } from '../../../graphql-types';
 
 import Layout from '@src/Layout';
 import { SEO } from '@components/layout';
 import { SubTitle, Sidebar, Title } from '@components/generic';
-import { MemberCard, LabLeadCard } from '@components/members';
+import { MemberCard } from '@components/members';
 
-import { labLeadData, memberGroupsData } from '@assets/data';
+import { alumnusGroupsData } from '@assets/data';
 
-interface MembersProps {
-  data: OtherMembersMdxQuery;
+interface AlumniProps {
+  data: OtherAlumniMdxQuery;
 }
 
-export default function Members({ data }: MembersProps) {
-  const otherMembers = data.mdx?.body;
+export default function Alumni({ data }: AlumniProps) {
+  const otherAlumniExist = Boolean(data.mdx?.rawBody);
+  const otherAlumni = data.mdx?.body;
 
-  const linkTitles: string[] = ['Lab Lead'].concat(
-    memberGroupsData.map(({ groupName }) => groupName)
+  const linkTitles: string[] = alumnusGroupsData.map(
+    ({ groupName }) => groupName
   );
-  if (otherMembers) linkTitles.push('Other Affiliated Members');
+  if (otherAlumniExist) linkTitles.push('Other Alumni');
 
   // sidebar link refs
   const linksRef = useRef<any>(linkTitles.map(() => createRef()));
 
   return (
     <Layout>
-      <SEO title={'Members'} />
+      <SEO title={'Alumni'} />
 
       <div className={'space-y-6'}>
-        <Title>Members</Title>
+        <Title>Alumni</Title>
 
         <Sidebar linksRef={linksRef} linkTitles={linkTitles}>
           <div className={'space-y-8'}>
-            {/* lab lead (json) */}
-            <div ref={linksRef.current[0]}>
-              <LabLeadCard labLead={labLeadData} />
-            </div>
-
-            {/* members (json) */}
+            {/* alumni (json) */}
             <div className={'space-y-12'}>
-              {memberGroupsData.map(({ groupName, members }, index) => (
-                <div ref={linksRef.current[index + 1]} key={index}>
+              {alumnusGroupsData.map(({ groupName, members }, index) => (
+                <div ref={linksRef.current[index]} key={index}>
                   <SubTitle>{groupName}</SubTitle>
                   <div className={'mt-4 space-y-8'}>
                     {members.map((member, index) => (
@@ -56,9 +52,9 @@ export default function Members({ data }: MembersProps) {
             </div>
 
             {/* members (mdx) */}
-            {otherMembers && (
+            {otherAlumniExist && otherAlumni && (
               <div ref={linksRef.current[linkTitles.length - 1]}>
-                <MDXRenderer>{otherMembers}</MDXRenderer>
+                <MDXRenderer>{otherAlumni}</MDXRenderer>
               </div>
             )}
           </div>
@@ -69,9 +65,10 @@ export default function Members({ data }: MembersProps) {
 }
 
 export const pageQuery = graphql`
-  query OtherMembersMdx {
-    mdx(fileAbsolutePath: { regex: "/OtherMembers/" }) {
+  query OtherAlumniMdx {
+    mdx(fileAbsolutePath: { regex: "/OtherAlumni/" }) {
       body
+      rawBody
     }
   }
 `;
